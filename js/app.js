ee0d743cbfce466d7033728f2cf7fa326b2bdec3
Rainer Brieger <hello@rainerb.com>
2020-08-25 14:41:42 -0600

    Finished other examples: finding avg of scores, finding a certain word within a tweet

--- js/app.js
+++ js/app.js
@@ -1,15 +1,10 @@
 // MORE PRACTICE EXAMPLES FROM ALEX
-//- A function that returns the middle element of an array. DONE
-//- A function that calculates the average of an array of user scores. DONE
-//- A function that will return all "tweets" that contain a word (meaning the function takes in an array of tweets and the word to search for). This one is actually pretty real world as it does what a real search function on a website does.
-
-
 
 // A function that returns the middle element of the array
 
-
+// Uncomment to test
 var arrayOfNumbers = [1, 2, 3, 4, 5, 6, 7];
-//var array = [1, 2, 3, 4, 5, 6];
+//var arrayOfNumbers = [1, 2, 3, 4, 5, 6];
 
 // Finding middle element of array
 function findMiddle(arrayOfNumbers) {
@@ -24,3 +19,70 @@ function findMiddle(arrayOfNumbers) {
 // Calling function
 findMiddle(arrayOfNumbers);
 
+
+
+// Twitter examples
+
+var userTwitter = [
+  {
+    tweet: "You need to GIT GUD",
+    username: "@rainerb16",
+    name: "Rainer",
+    score: 9.5
+  },
+  {
+    tweet: "HELLO?! Hooman, I want food! NOW!",
+    username: "@meow",
+    name: "Cylus",
+    score: 8
+  },
+  {
+    tweet: "HELLO?! I'm sleepy deepy.",
+    username: "@brandelio",
+    name: "Brandy",
+    score: 10
+  },
+  {
+    tweet: "ZEUS, stop it! You're going to get us in trouble!",
+    username: "@ibeatzeus",
+    name: "Cola",
+    score: 4.5
+  },
+  {
+    tweet: "Cola, stop tattling on me!",
+    username: "@kingzeus",
+    name: "Zeus",
+    score: 3
+  }
+];
+
+// Calculate average score of users
+function calculateAvg(userArray) {
+    var userSum = 0;
+    for(var i = 0; i < userArray.length; i++) {
+        userSum += userArray[i].score;
+    }
+    var userAvg = userSum / userArray.length;
+    return userAvg;
+}
+
+// Calling function
+var userAvgResult = calculateAvg(userTwitter);
+console.log(userAvgResult);
+
+
+
+// Function that return all "tweets" that contain a certain word
+function tweetSearch(userTweets, word) {
+    var matchTweets = [];
+    for(var i = 0; i < userTweets.length; i++) {
+        if(userTweets[i].tweet.includes(word)) {
+            matchTweets.push(userTweets[i]);
+        }
+    }
+    return matchTweets;
+}
+
+// Calling function
+var tweetsFound = tweetSearch(userTwitter,"HELLO");
+console.log(tweetsFound);
